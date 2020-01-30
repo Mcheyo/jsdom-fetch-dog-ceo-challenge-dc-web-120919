ee1cbb20975dcce07ec73529d1f018194331a2ce
Mansoor  Cheyo <mcheyo.mc@gmail.com>
2020-01-30 15:50:04 -0500

    ok

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ console.log('%c HI', 'color: firebrick')
 
 document.addEventListener("DOMContentLoaded", () => { 
     console.log("connected")
-    console.log()
+    liColor().addEventListener('click', changeColor)
+    getId().addEventListener('click', grabDropdown )
     renderDogs()
     renderBreeds()
+
 })
 
+function liColor() {
+return document.getElementById("dog-breeds")
+}
 
+function getId(){ 
+    return document.getElementById("breed-dropdown")
+}
 let renderDogs = function() { 
     fetch ('https://dog.ceo/api/breeds/image/random/4')
     .then( response => response.json() )
@@ -52,8 +60,28 @@ function breedList (breed){
         })
         list.appendChild(breedLi)
     })
-
-    
-   
-    
 }
+
+function changeColor (e){ 
+    
+    let colorchange = e.target
+    colorchange.style.color="red"
+
+}
+
+// function grabDropdown (){ 
+    
+//     let test =  document.getElementById("breed-dropdown")
+//     test.onchange = function (e){ 
+//         let list = document.getElementById("dog-breeds")
+        
+//          for (let i = 0; i < list.childNodes.length; i++){
+//             matchUl = document.createElement('ul')
+//             matchLi = document.createElement('li')
+//             matchLi.innerText = list.childNodes.textContent
+//             matchUl.appendChild(matchLi)
+//             list.appendChild(matchUl)
+//         }
+       
+//     }
+// }
